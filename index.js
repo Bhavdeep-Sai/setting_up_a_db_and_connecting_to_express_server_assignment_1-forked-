1cdd8bbff1787d060492569a58a5d7ce75c5ad4a
Bhavdeep-Sai <bhavdeepsai@gmail.com>
2025-02-24 09:08:31 +0530

    seting up mongodb connection to the server

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+
+
+const DB_URL = 'mongodb+srv://emparalabhavdeeps86:Mto6FYnqiMjsgKGo@practiceassignment1.monm6.mongodb.net/?retryWrites=true&w=majority&appName=practiceAssignment1';
+
+mongoose.connect(DB_URL)
+.then(()=>{console.log('Mongodb connected Successfully!')})
+.catch(err => console.log('Mongodb connection failed : ',err));
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
